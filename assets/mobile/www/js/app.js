27239ba60c2de920c3a6d8e25dd82025e7883e75
Александр Винников <alvin@MacBook-Air-Aleksandr.local>
2019-01-09 13:16:30 +0300

    login screen

--- assets/mobile/www/js/app.js
+++ assets/mobile/www/js/app.js
@@ -1,19 +1,18 @@
 // Dom7
 var $$ = Dom7;
+var username = null;
 
 // Framework7 App main instance
 var app  = new Framework7({
   root: '#app', // App root element
-  id: 'io.framework7.testapp', // App bundle ID
-  name: 'Framework7', // App name
+  id: 'ru.ac.mpei.mm.aal', // App bundle ID
+  version: '1.0.0',
+  name: 'MPEI AAL', // App name
   theme: 'auto', // Automatic theme detection
   // App root data
   data: function () {
     return {
-      user: {
-        firstName: 'John',
-        lastName: 'Doe',
-      },
+      username
     };
   },
   // App root methods
@@ -41,13 +40,35 @@ var mainView = app.views.create('.view-main', {
 });
 
 // Login Screen Demo
-$$('#my-login-screen .login-button').on('click', function () {
-  var username = $$('#my-login-screen [name="username"]').val();
-  var password = $$('#my-login-screen [name="password"]').val();
+$$('.login-screen-content .login-button').on('click', function () {
+  app.input.validateInputs('.login-screen-content');
+  
+  var username = $$('.login-screen-content [name="username"]:valid').val();
+  var password = $$('.login-screen-content [name="password"]:valid').val();
+  var server = $$('.login-screen-content [name="url"]:valid').val() || 'http://mm.mpei.ac.ru:8080';
 
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
+  app.preloader.show();
+  $.ajax({
+    type: "POST",
+    url: server+"/login",
+    data: {username, password},
+    timeout: 1500,
+    success: function(data, textStatus) {
+        if (data.redirect) {
+            // data.redirect contains the string URL to redirect to
+            console.log('ok',data);
+        }
+        else {
+            // data.form contains the HTML for the replacement form
+            console.log('err',data);
+        }
+        app.preloader.hide();
+    },
+    error: function(data, textStatus) {
+      app.dialog.alert(textStatus=="timeout"?"Сервер не отвечает.":"Ошибка при обращении к серверу.");
+      app.preloader.hide();
+    }
+  });
 
   // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
 });
