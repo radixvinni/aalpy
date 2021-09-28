c7460a5015c02d83738d1834bbbe089cd9ebad1f
user <user@local>
2021-09-28 20:51:47 +0300

    sppm

--- maude/sppm.py
+++ maude/sppm.py
@@ -3,24 +3,24 @@
 
 import argparse
 import signal
-from os import path, kill
+from os import path, kill, remove
 from subprocess import Popen
 from prettytable import PrettyTable
 from psutil import pid_exists
 from tinydb import TinyDB, Query
+import sys
 
 # Usage help
 help = """
-Simple Python Process Manager
+python sppm.py <command>
 
-sppm <command>
+Simple Maude Process Manager
 
 Available commands:
 help - show this help message
-start <file/taskid/all> [args] - start the process with optional arguments
+start <command> - start the process with optional arguments
 stop <taskid/all> - stop the process
-restart <taskid/all> [args] - restart the process with optional arguments
-remove <taskid> - remove the process from the list
+remove <taskid/all> - remove the process from the list
 list - list all processes"""
 
 # == Main Functions ==
@@ -28,9 +28,14 @@ list - list all processes"""
 db = TinyDB("/tmp/sppm_db.json")
 rpTable = db.table("running_processes")
 Process = Query()
- 
 
+
+exe = 'echo ""|LD_LIBRARY_PATH=/chroot/oldstable/root/maude/ /chroot/oldstable/lib/ld-linux.so.2 /chroot/oldstable/root/maude/maude -no-banner %s > %s 2>&1'
+if sys.platform == 'darwin':
+    exe = 'echo ""|./maude31/maude.darwin -no-banner %s > %s 2>&1'
 # Simple error formatting with usage help
+
+
 def sppm_error(str):
     print(help + "\n\nERROR: " + str)
     quit()
@@ -45,24 +50,26 @@ def process_kill(proc):
 
 # Function for the "stop" command
 def stop_process(cmdargs):
+    if isinstance(cmdargs, list):
+        cmdargs = cmdargs[0]
     if not cmdargs:  # Check if arguments are missing after stop
         sppm_error("Missing taskid on \"stop\"")
     else:
         try:
             # Check if argument is "all"
-            if cmdargs[0] == "all":
+            if cmdargs == "all":
                 # Run kill on all processes
                 for proc in rpTable.all():
                     if int(proc["running"]) == 1:
                         process_kill(proc)
 
             # Else, check if process exists by taskid
-            elif rpTable.get(Process.id == int(cmdargs[0])):
+            elif rpTable.get(Process.id == int(cmdargs)):
                 # Check if it's running, then kill
-                if int(rpTable.get(Process.id == int(cmdargs[0]))["running"]) == 0:
+                if int(rpTable.get(Process.id == int(cmdargs))["running"]) == 0:
                     print("Process is not running.")
                 else:
-                    process_kill(rpTable.get(Process.id == int(cmdargs[0])))
+                    process_kill(rpTable.get(Process.id == int(cmdargs)))
 
             else:
                 sppm_error("Not a valid taskid")
@@ -75,89 +82,100 @@ def stop_process(cmdargs):
 # Function for the "list" command
 def list_processes():
     # Simple table output using PrettyTable
-    print("\nSimple Python Process Manager: Process List")
+    print("\nSimple Maude Process Manager: Process List")
     outTable = PrettyTable()
     outTable.hrules = True
-    outTable.field_names = ["ID", "Name", "Args", "Running", "PID", "Path"]
+    outTable.field_names = ["ID", "Running", "PID", "Command"]
     # Add a PrettyTable row for each row of the rpTable
     for proc in rpTable.all():
-        outTable.add_row([proc["id"], proc["name"], proc["args"], "Yes" if proc["running"] == 1 else "No",
+        outTable.add_row([proc["id"], "Yes" if proc["running"] == 1 else "No",
                           proc["pid"], proc["path"]])
     print(outTable)
 
 
 # Function for the "start" command
+
+
 def start_process(cmdargs):
+    if isinstance(cmdargs, list):
+        cmdargs = cmdargs[0]
     if not cmdargs:  # Check if arguments are missing after start
         sppm_error("Missing arguments on \"start\"")
-    elif path.exists(cmdargs[0]):  # Check if valid path
-        if path.isfile(cmdargs[0]):  # Check if path is a file
+    elif isinstance(cmdargs, str) and not cmdargs.isdigit():
+        procPath = (cmdargs)
+        procArgs = ""
+        filename = ""
 
-            # File info vars
-            procPath = path.abspath(cmdargs[0])
-            procArgs = " ".join(cmdargs[1:])
-            filename = path.basename(procPath)
-
-            # Check if process exists on DB and if it's already running or not
-            if rpTable.get((Process.path == procPath) & (Process.args == procArgs)):
-                res = rpTable.get((Process.path == procPath) & (Process.args == procArgs))
-                if int(res["running"]) == 1:
-                    sppm_error("Process is already running with taskid " + str(res["id"]))
-                else:  # If it exists on DB but isn't running
-                    # Start new process and get PID
-                    proc = Popen(["python"] + cmdargs, stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
-                    procPid = proc.pid
-
-                    # Update DB entry PID and status
-                    rpTable.update({"pid": procPid, "running": 1},
-                                   (Process.path == procPath) & (Process.args == procArgs))
-                    print("Process started with taskid " + str(res["id"]))
-            else:  # If not on DB
-                # Start new process and get PID
-                proc = Popen(["python"] + cmdargs, stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
+        # Check if process exists on DB and if it's already running or not
+        if rpTable.get((Process.path == procPath) & (Process.args == procArgs)):
+            res = rpTable.get((Process.path == procPath)
+                              & (Process.args == procArgs))
+            if int(res["running"]) == 1:
+                sppm_error(
+                    "Process is already running with taskid " + str(res["id"]))
+            else:  # If it exists on DB but isn't running
+                """# Start new process and get PID
+                proc = Popen(["python"] + cmdargs, stdin=DEVNULL,
+                             stdout=DEVNULL, stderr=DEVNULL)
                 procPid = proc.pid
 
-                # Insert or update process on the running_processes table
-                biggestID = 0
-                for proc in rpTable.all():
-                    if int(proc["id"]) > biggestID:
-                        biggestID = int(proc["id"])
-
-                rpTable.upsert({"id": biggestID + 1, "name": filename, "pid": procPid, "path": procPath,
-                                "args": " ".join(cmdargs[1:]), "running": 1},
+                # Update DB entry PID and status
+                rpTable.update({"pid": procPid, "running": 1},
                                (Process.path == procPath) & (Process.args == procArgs))
-                print("Process started with taskid " + str(biggestID + 1))
-        else:
-            sppm_error("Path is not a valid file \"" + cmdargs[0] + "\"")
+                print("Process started with taskid " + str(res["id"]))"""
+                print("Process with taskid " +
+                      str(res["id"]) + " stopped with output:")
+                print(open('/tmp/out'+str(res["id"]), 'r').read())
+        else:  # If not on DB
+            # Insert or update process on the running_processes table
+            biggestID = 0
+            for proc in rpTable.all():
+                if int(proc["id"]) > biggestID:
+                    biggestID = int(proc["id"])
+
+            biggestID = biggestID + 1
+            infile = '/tmp/in' + str(biggestID)
+            outfile = '/tmp/out' + str(biggestID)
+            open(infile, 'w').write(cmdargs)
+            # Start new process and get PID
+            proc = Popen(['/bin/bash', '-c', exe % (infile, outfile)])
+            procPid = proc.pid
+
+            rpTable.upsert({"id": biggestID, "name": filename, "pid": procPid, "path": procPath,
+                            "args": procArgs, "running": 1},
+                           (Process.path == procPath) & (Process.args == procArgs))
+            print("Process started with taskid " + str(biggestID))
 
     # Not a valid path, user probably entered taskid
     else:
         # Exception catching for invalid arguments
         try:
-            # Check if argument is "all"
-            if cmdargs[0] == "all":
+            """# Check if argument is "all"
+            if cmdargs == "all":
                 # Run the process for every entry in rpTable if it's not already running
                 for proc in rpTable.all():
                     if int(proc["running"]) == 0:
                         process = Popen(["python"] + [proc["path"]] + proc["args"].split(" "),
                                         stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
-                        rpTable.update({"running": 1, "pid": process.pid}, Process.id == proc["id"])
+                        rpTable.update(
+                            {"running": 1, "pid": process.pid}, Process.id == proc["id"])
                         print("Process started with taskid " + str(proc["id"]))
-
+            """
             # Check the existence of a process with the given taskid
-            elif rpTable.get(Process.id == int(cmdargs[0])):
-                id = int(cmdargs[0])
+            if rpTable.get(Process.id == int(cmdargs)):
+                id = int(cmdargs)
                 res = rpTable.get(Process.id == id)
 
                 # Check if running
                 if int(res["running"]) == 0:
-                    # Check if file still exists
+                    """# Check if file still exists
                     if path.isfile(res["path"]):
-
                         # Start the process
-                        cmdargs[0] = res["path"]
-                        proc = Popen(["python"] + cmdargs, stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
-                        rpTable.update({"running": 1, "pid": proc.pid}, Process.id == id)
+                        cmdargs = res["path"]
+                        proc = Popen(["python"] + cmdargs, stdin=DEVNULL,
+                                     stdout=DEVNULL, stderr=DEVNULL)
+                        rpTable.update(
+                            {"running": 1, "pid": proc.pid}, Process.id == id)
                         print("Process started with taskid " + str(res["id"]))
 
                     else:  # File doesn't exist. Query the user on whether to remove entry or not
@@ -169,57 +187,69 @@ def start_process(cmdargs):
                                 print("Invalid option.")
                         if deleteInput == "y":
                             remove_process(str(res["id"]))
-
+                    """
+                    print("Process stopped")
+                    print(open('/tmp/out'+str(res["id"]), 'r').read())
                 else:
                     sppm_error("Process is already running")
-
             else:  # User gave an invalid path or taskid
-                sppm_error("Not a valid path or taskid \"" + str(cmdargs[0]) + "\"")
+                sppm_error("Not a valid path or taskid \"" +
+                           str(cmdargs) + "\"")
 
         except ValueError:  # Got a value error, argument is probably wrong
             sppm_error("Invalid argument for taskid or path")
 
 
+def remove_proc(i):
+    remove('/tmp/out' + str(i))
+    remove('/tmp/in' + str(i))
+    rpTable.remove(Process.id == int(i))
+    print("Removed process with taskid " + str(i))
+
 # Function for the "remove" command
+
+
 def remove_process(cmdargs):
+    if isinstance(cmdargs, list):
+        cmdargs = cmdargs[0]
     try:
-        if rpTable.get(Process.id == int(cmdargs[0])):
-            docid = rpTable.get(Process.id == int(cmdargs[0])).doc_id
-            rpTable.remove(Process.id == int(cmdargs[0]))
-            print("Removed process with taskid " + cmdargs[0])
+        if cmdargs == "all":
+            stop_process("all")
+            for proc in rpTable.all():
+                remove_proc(proc["id"])
+        elif rpTable.get(Process.id == int(cmdargs)):
+            remove_proc(cmdargs)
     except ValueError:
         sppm_error("Invalid argument for taskid")
 
-if __name__ == '__main__':
- # Argument parsing
- parser = argparse.ArgumentParser(description="Simple Python Process Manager", add_help=False, usage=help)
- parser.add_argument("command", choices=["list", "start", "stop", "restart", "help", "remove"])
- parser.add_argument("args", nargs=argparse.REMAINDER)
- args = parser.parse_args() 
- cmdargs = args.args
- 
- # Check status of all saved processes and update entry if the process is not running
- for proc in rpTable.all():
-    if int(proc["running"]) == 1:
-        if not pid_exists(int(proc["pid"])):
-            rpTable.update({"running": 0}, Process.pid == int(proc["pid"]))
- # == Command Redirection ==
- if args.command == "help":
-    print(help)
 
- elif args.command == "start":
-    start_process(cmdargs)
- 
- elif args.command == "list":
-    list_processes()
- 
- elif args.command == "stop":
-    stop_process(cmdargs)
- 
- elif args.command == "restart":
-    print("Restarting...")
-    stop_process(cmdargs)
-    start_process(cmdargs)
- 
- elif args.command == "remove":
-    remove_process(cmdargs)
+if __name__ == '__main__':
+    # Argument parsing
+    parser = argparse.ArgumentParser(
+        description="Simple Maude Process Manager", add_help=False, usage=help)
+    parser.add_argument("command", choices=[
+                        "list", "start", "stop", "restart", "help", "remove"])
+    parser.add_argument("args", nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+    cmdargs = args.args
+
+    # Check status of all saved processes and update entry if the process is not running
+    for proc in rpTable.all():
+        if int(proc["running"]) == 1:
+            if not pid_exists(int(proc["pid"])):
+                rpTable.update({"running": 0}, Process.pid == int(proc["pid"]))
+    # == Command Redirection ==
+    if args.command == "help":
+        print(help)
+
+    elif args.command == "start":
+        start_process(cmdargs)
+
+    elif args.command == "list":
+        list_processes()
+
+    elif args.command == "stop":
+        stop_process(cmdargs)
+
+    elif args.command == "remove":
+        remove_process(cmdargs)
